4882a43c02fdcc454c48b03c313ac124b2f66546
denys09310 <101510131+denys09310@users.noreply.github.com>
2024-03-24 17:28:50 +0200

    Add files via upload

--- gedigedao/main.py
+++ gedigedao/main.py
@@ -1,9 +1,41 @@
 from PIL import Image, ImageFilter
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow, QFileDialog, QMessageBox
+from ui import Ui_MainWindow
+import os 
 
-with Image.open('11.jpg') as original:
-    pic_gray = original.convert('L')
-    pic_blur = original.filter(ImageFilter.BLUR)
+class Widget(QMainWindow):
+    def   __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.workdir = None
+        self.filenames = None
+        self.image = None
+        self.ui.floder_btn.clicked.connect(self.choose_floder)
 
-    pic_gray.save('negr.jpg')
-    pic_blur.save('gray.jpg')
-    pic_gray.show()
+    def show_image_list(self):
+        self.filenames = os.listdir(self.workdir)
+        images = []
+        for filename in self.filenames:
+            if filename.endswith('.png') or filename.endswith(".jpg") or filename.endswith("jpeg"):
+                images.append(filename)
+        self.ui.image_list.clear()
+        self.ui.image_list.addItems(images)
+
+        self.ui.image_list.addItems(images)
+
+    def choose_floder(self):
+        try:
+            self.workdir = QFileDialog.getExistingDirectory()
+            self.show_image_list()
+        except:
+            message = QMessageBox()
+            message.setText("ERROR kfvjbhafiaiwFDGladfjsdkajfjljgfjgdsabjcvfhjwvDFKJGEjkgdsJKFd;uoidufsdhukfsdIFL:Y;yiosdFuDFS;ugidfs;oufsd:UfsDHLIfS:HKFSH:I;oiysdfugioDSFg;usdfg;usDFiug;dsfU:GIDSFGUo;sdF:HUf:UHdf:HUd;hFhu;xcjzhjchisdfheuwfguisuGIGUISDHUUGFEWGYUDYUFEUWFVEUFyufGUIAIUGHGugifeauoigruhrifetven")
+            message.exec_()
+
+app = QApplication([])
+ex = Widget()
+ex.show()
+app.exec_()
+
